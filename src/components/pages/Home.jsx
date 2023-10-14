48100d485886a281d6f42504c9966903fe63aa30
imadbenmadi <imadbenmadi891@gmail.com>
2023-10-14 16:33:49 +0100

    test

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import OurGoals from '../homeComponenets/OurGoals'
+function Home() {
+  return (
+    <div>
+      {/* navbar */}
+      {/* header */}
+      {/* our golas */}<OurGoals />
+      {/* members */}
+      {/* quote */}
+      {/* footer */}
+    </div>
+  )
+}
+
+export default Home
